afb59085c1e5be4c8ef2d1c54f414cfc374efcc4
tyche <tylerclargen@gmail.com>
2020-10-05 23:43:21 -0400

    Fix everything you broke

--- src/main/java/com/nylanomel/frontiers/Frontiers.java
+++ src/main/java/com/nylanomel/frontiers/Frontiers.java
@@ -1,18 +1,31 @@
 package com.nylanomel.frontiers;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Frontiers implements ModInitializer {
 
-	public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier("frontiers", "general"),
+			() -> new ItemStack(Blocks.COBBLESTONE));
+
+	public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(
+			new Identifier("frontiers", "other"))
+			.icon(() -> new ItemStack(Items.BOWL))
+			.build();
+
+	public static final Item FABRIC_ITEM = new Item(new Item.Settings().group(Frontiers.ITEM_GROUP));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
+		Registry.register(Registry.ITEM, new Identifier("frontiers", "fabric_item"), FABRIC_ITEM);
 	}
 }
--- src/main/java/com/nylanomel/frontiers/mixin/FMixin.java
+++ src/main/java/com/nylanomel/frontiers/mixin/FMixin.java
@@ -1,7 +1,6 @@
 package com.nylanomel.frontiers.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
